ee5301514424e216a9b602817e879d06bf56ea3b
Scott Smith <smitherz82@gmail.com>
2024-01-09 12:28:35 +0000

    Added local loading and danger button that was removed from Nova (#15)

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,5 +1,7 @@
+import CommandRunnerDangerButton from './components/DangerButton'
 import Tool from './pages/Tool'
 
 Nova.booting((app, store) => {
+  Nova.component('CommandRunnerDangerButton', CommandRunnerDangerButton)
   Nova.inertia('NovaCommandRunner', Tool)
 })
